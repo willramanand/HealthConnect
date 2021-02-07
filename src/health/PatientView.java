9e57865812ad9203ec24fa2f141c7c6cf8cc819a
William Ramanand <willramanand@gmail.com>
2021-02-07 18:37:18 -0500

    Program is now running added a rough schema based off of the code.

--- src/health/PatientView.java
+++ src/health/PatientView.java
@@ -33,7 +33,7 @@ public final class PatientView extends javax.swing.JFrame {
         initComponents();
         try {
             Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Andrea\\Documents\\NetBeansProjects\\health.sqlite");
+            conn = DriverManager.getConnection("jdbc:sqlite:db\\health");
             //JOptionPane.showMessageDialog (null, "Connected");
             Statement statement = conn.createStatement();
         }
